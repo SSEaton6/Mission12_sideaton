b869ab2007b77ce70b18be09a2ba3d75f6f4dba6
Sidney Eaton <sideaton@byu.edu>
2023-03-22 23:26:02 -0600

    8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 // const schoolinfo = require('CollegeBasketballTeams.json'); //with path
+// import the data from the JSON file
 import * as schoolInfo from '../src/CollegeBasketballTeams.json';
 
+//function to make an heading to Welcome users to the site
 function Welcome() {
   return (<h1>Welcome! This page lists out schools, mascots, and locations</h1>)
 }
 
-
+//Class to create the Team card
 class Team extends React.Component <{name: string; school: string; city: string; state: string}>
 {
   render() {
@@ -25,7 +27,7 @@ class Team extends React.Component <{name: string; school: string; city: string;
   }
 }
 
-
+//unneeded. Originally planned to also list out just the school names as well as the cards
 class School extends React.Component <{name: string; school: string; city: string; state: string}>
 {
   render() {
@@ -38,6 +40,7 @@ class School extends React.Component <{name: string; school: string; city: strin
   }
 }
 
+// functions to create the divs to display the data for each of the classes
 function TeamList(){
   return(
     <div>
@@ -53,6 +56,7 @@ function SchoolList(){
   )
 }
 
+// function to display everything on the page
 
 function App(){
   return (
@@ -63,5 +67,5 @@ function App(){
     </div>
   )
 }
-
+// calls the App function and displays info.
 export default App;
